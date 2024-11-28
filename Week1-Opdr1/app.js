316d22d0c1ab2d40cfa02ad9f5389f20f02a9e94
Suhailism49 <9022614@student.zadkine.nl>
2024-11-28 10:00:09 +0100

    Add files via upload
    Opdrcht1 week1 v1

---
+++ Week1-Opdr1/app.js
@@ -0,0 +1,4 @@
+document.getElementById('accept-cookies').addEventListener('click', function() {
+    document.getElementById('cookie-wall').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+});
